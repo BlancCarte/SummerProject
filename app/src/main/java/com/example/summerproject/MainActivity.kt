a47d2b74fac80e974bb60c97eac7d60e4f7e0c93
Beomseok <beso1234@naver.com>
2021-07-09 20:42:17 +0900

    BBS 프론트 1주차 합치기

--- app/src/main/java/com/example/summerproject/MainActivity.kt
+++ app/src/main/java/com/example/summerproject/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.summerproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.summerproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
