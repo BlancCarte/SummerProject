9bad8858a32ab446608b27bb2c07cc7aae580fa4
Beomseok <beso1234@naver.com>
2021-07-10 00:13:46 +0900

    BBS 7/9 프론트 작성본

--- app/src/main/java/com/example/summerproject/HomeActivity.kt
+++ app/src/main/java/com/example/summerproject/HomeActivity.kt
@@ -1,11 +1,14 @@
-package com.example.androidterm
+package com.example.summerproject
+
 
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ActionBarContextView
 import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,18 +16,22 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.summerproject.R
 import com.example.summerproject.databinding.ActivityHomeBinding
-import com.example.summerproject.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity  : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_home_black_24dp)  // 왼쪽 버튼 아이콘 설정
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+
 
         val navView: BottomNavigationView = binding.navView
 
@@ -46,8 +53,12 @@ class HomeActivity : AppCompatActivity() {
 
     //액션버튼 클릭 했을 때
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId){
-            R.id.action_search -> {
+        when(item!!.itemId){
+            android.R.id.home->{ // 메뉴 버튼
+                Toast.makeText(applicationContext, "메뉴 이벤트 실행", Toast.LENGTH_SHORT).show()
+                return super.onOptionsItemSelected(item)
+            }
+            R.id.action_search-> {
                 //검색 버튼 눌렀을 때
                 Toast.makeText(applicationContext, "검색 이벤트 실행", Toast.LENGTH_LONG).show()
                 return super.onOptionsItemSelected(item)
--- app/src/main/java/com/example/summerproject/MainActivity.kt
+++ app/src/main/java/com/example/summerproject/MainActivity.kt
@@ -18,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
