d87e1fd2112a73b28528125843977921832c50ba
drdd1120 <drdd1120@naver.com>
2021-07-11 02:02:43 +0900

    모든 회원가입 유효성 검사 완료(색 추가)

--- app/src/main/java/com/example/summerproject/SignUpActivity.kt
+++ app/src/main/java/com/example/summerproject/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.example.summerproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -14,7 +15,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 private var firebaseAuth: FirebaseAuth? = null
-private var firebaseFirestore : FirebaseFirestore? = null
+private var firebaseFirestore: FirebaseFirestore? = null
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -28,16 +29,72 @@ class SignUpActivity : AppCompatActivity() {
 		setContentView(binding.root)
 		supportActionBar?.hide();
 
-		//비밀번호 -> 비밀번호 확인 검사
-		binding.passwordConfirm.addTextChangedListener(object : TextWatcher {
-
+		//이메일 유효성검사
+		binding.email.addTextChangedListener(object: TextWatcher{
 			override fun afterTextChanged(p0: Editable?) {
-				if(binding.password.text.toString().equals(binding.passwordConfirm.text.toString()) && binding.password.length() >= 8){
-				   binding.btnRegister.isEnabled = true
-					binding.passwordCheckText.setText("")
+				var regex = Regex("[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}")
+				var email = binding.email.text.toString()
+
+				if(email.matches(regex)){
+					binding.btnRegister.isEnabled = true
+					binding.emailTest.setTextColor(Color.parseColor("#369F36"))
+					binding.emailTest.setText("이메일이 입력되었습니다.")
 				}else{
-				   binding.btnRegister.isEnabled = false
-					binding.passwordCheckText.setText("비밀번호가 일치하지 않습니다")
+					binding.btnRegister.isEnabled = false
+					binding.emailTest.setTextColor(Color.parseColor("#ff0000"))
+					binding.emailTest.setText("이메일을 형식에 맞춰 입력해주세요")
+
+				}
+			}
+			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+		})
+
+		//닉네임 유효성검사
+		binding.nickname.addTextChangedListener(object: TextWatcher{
+			override fun afterTextChanged(p0: Editable?) {
+				var regex = Regex("[가-힣a-zA-Z0-9]{2,10}")
+				var nickname = binding.nickname.text.toString()
+
+				if(nickname.matches(regex)){
+					binding.btnRegister.isEnabled = true
+					binding.nicknameTest.setTextColor(Color.parseColor("#369F36"))
+					binding.nicknameTest.setText("별명이 입력되었습니다.")
+				}else{
+					binding.btnRegister.isEnabled = false
+					binding.nicknameTest.setTextColor(Color.parseColor("#ff0000"))
+					binding.nicknameTest.setText("별명을 형식에 맞춰 입력해주세요.")
+				}
+			}
+			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+		})
+
+		//비밀번호 -> 비밀번호 확인 검사
+		binding.password.addTextChangedListener(object : TextWatcher {
+			override fun afterTextChanged(p0: Editable?) {
+				var regex = Regex("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@!%*#?&]).{8,15}")
+				var pw = binding.password.text.toString()
+
+				if(pw.matches(regex)){
+					binding.btnRegister.isEnabled = true
+					binding.passwordCheckText.setTextColor(Color.parseColor("#369F36"))
+					binding.passwordCheckText.setText("비밀번호가 입력되었습니다.")
+					if (binding.passwordConfirm.text.toString()==(binding.password.text.toString())){
+						binding.btnRegister.isEnabled = true
+						binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
+						binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
+					}
+				}
+				else{
+					binding.btnRegister.isEnabled = false
+					binding.passwordCheckText.setTextColor(Color.parseColor("#ff0000"))
+					binding.passwordCheckText.setText("비밀번호를 형식에 맞춰 입력해주세요.")
+					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
+
 				}
 			}
 
@@ -48,15 +105,44 @@ class SignUpActivity : AppCompatActivity() {
 
 		//비밀번호 확인 -> 비밀번호 검사
 		//TextWatcher : 원하는 규칙에 대해서 실시간으로 검증하기 위해
-		binding.password.addTextChangedListener(object : TextWatcher {
 
+		binding.passwordConfirm.addTextChangedListener(object : TextWatcher {
 			override fun afterTextChanged(p0: Editable?) {
-				if(binding.password.text.toString().equals(binding.passwordConfirm.text.toString()) && binding.passwordConfirm.length() >= 8){
+				var pw = binding.password.text.toString()
+				if(binding.passwordConfirm.text.toString()==(binding.password.text.toString())){
 					binding.btnRegister.isEnabled = true
-					binding.passwordCheckText.setText("")
-				}else{
+					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
+					binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
+				}else if(!(binding.passwordConfirm.text.toString()==(binding.password.text.toString()))){
+					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
+				}
+				else{
 					binding.btnRegister.isEnabled = false
-					binding.passwordCheckText.setText("비밀번호가 일치하지 않습니다")
+					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
+				}
+			}
+
+			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+		})
+
+		//전화번호
+		binding.phoneNumber.addTextChangedListener(object : TextWatcher{
+			override fun afterTextChanged(p0: Editable?) {
+				var phnum = binding.phoneNumber.text.toString()
+				var regex = Regex("01[016789][0-9]{3,4}[0-9]{4}$")
+				if(phnum.matches(regex)){
+					binding.btnRegister.isEnabled = true
+					binding.phoneNumberTest.setTextColor(Color.parseColor("#369F36"))
+					binding.phoneNumberTest.setText("입력되었습니다.")
+				}
+				else{
+					binding.btnRegister.isEnabled = false
+					binding.phoneNumberTest.setTextColor(Color.parseColor("#ff0000"))
+					binding.phoneNumberTest.setText("핸드폰 형식이 아닙니다.")
 				}
 			}
 
@@ -66,12 +152,12 @@ class SignUpActivity : AppCompatActivity() {
 		})
 
 		// 게터세터
-		data class UserDTO (
-			var uid :String? = null,
-			var email :String? = null,
-			var password :String? = null,
-			var nickname : String? = null,
-			var phoneNumber : String? = null
+		data class UserDTO(
+			var uid: String? = null,
+			var email: String? = null,
+			var password: String? = null,
+			var nickname: String? = null,
+			var phoneNumber: String? = null
 		)
 
 		binding.btnRegister.setOnClickListener {
@@ -86,7 +172,8 @@ class SignUpActivity : AppCompatActivity() {
 				.addOnCompleteListener(this) {
 					if (it.isSuccessful) {
 						val user = firebaseAuth?.currentUser
-						firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)?.document(userDTO.nickname!!)?.set(userDTO)
+						firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)
+							?.document(userDTO.nickname!!)?.set(userDTO)
 						Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
 
 						val intent = Intent(this, MainActivity::class.java)
