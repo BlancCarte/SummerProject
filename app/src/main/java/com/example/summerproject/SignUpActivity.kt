84b4a71ee902605f71cd16b4952344ed0ee40c75
BlancCarte <yusung0924@naver.com>
2021-07-10 22:16:59 +0900

    로그인 완료

--- app/src/main/java/com/example/summerproject/SignUpActivity.kt
+++ app/src/main/java/com/example/summerproject/SignUpActivity.kt
@@ -14,8 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 private var firebaseAuth: FirebaseAuth? = null
-private var firebaseFirestore: FirebaseFirestore? = null
-
+private var firebaseFirestore : FirebaseFirestore? = null
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -33,12 +32,13 @@ class SignUpActivity : AppCompatActivity() {
 		binding.passwordConfirm.addTextChangedListener(object : TextWatcher {
 
 			override fun afterTextChanged(p0: Editable?) {
-				binding.btnRegister.isEnabled = binding.password.text.toString() == binding.passwordConfirm.text.toString() && binding.password.length() >= 7
+				binding.btnRegister.isEnabled = binding.password.text.toString() == binding.passwordConfirm.text.toString() && binding.password.length() >= 8
 				/*if(binding.password.text.toString().equals(binding.passwordConfirm.text.toString()) && binding.password.length() >= 7){
-					binding.btnRegister.isEnabled = true
-				}else{
-					binding.btnRegister.isEnabled = false
-				}*/
+                   binding.btnRegister.isEnabled = true
+                }else{
+                   binding.btnRegister.isEnabled = false
+                }*/
+
 			}
 
 			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -74,7 +74,6 @@ class SignUpActivity : AppCompatActivity() {
 			var phoneNumber : String? = null
 		)
 
-
 		binding.btnRegister.setOnClickListener {
 			var userDTO = UserDTO()
 			userDTO.uid = firebaseAuth?.currentUser?.uid
@@ -87,9 +86,8 @@ class SignUpActivity : AppCompatActivity() {
 				.addOnCompleteListener(this) {
 					if (it.isSuccessful) {
 						val user = firebaseAuth?.currentUser
+						firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)?.document()?.set(userDTO)
 						Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
-						firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)?.document(userDTO.nickname!!)?.set(userDTO)
-
 						val intent = Intent(this, MainActivity::class.java)
 						startActivity(intent)
 
@@ -106,12 +104,11 @@ class SignUpActivity : AppCompatActivity() {
 // 입력한 값이 파이어베이스에 들어가야 하니까 에딧텍스트에 입력 가능하게 해야함 (O)
 // 에딧 텍스트를 찾아서 온클릭 리스너를 적용 시켜야 한다 (O)
 // 이메일 비밀번호 전화번호 닉네임 유효성 검사
-// 닉네임이랑 전화번호는 쿼리문 사용해서 파이어스토어 디비에 넣기
-// 회원가입 성공시 로그인 페이지로 돌아가서 다시 로그인 시키기
+// 닉네임이랑 전화번호는 쿼리문 사용해서 파이어스토어 디비에 넣기 (O)
+// 회원가입 성공시 로그인 페이지로 돌아가서 다시 로그인 시키기 (O)
 // 이메일 인증
-// 전화번호는 뺄지 말지 고민
 
-// 회원가입시 파이어스토어에 닉네임과 전화번호 보내기
+// 회원가입시 파이어스토어에 닉네임과 전화번호 보내기 (O)
 // 로그인
 // 비밀번호 정규식 검사
 // 추가 : 게시판
---
+++ app/src/main/java/com/example/summerproject/logintest.kt
@@ -0,0 +1,13 @@
+package com.example.summerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class logintest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logintest)
+    }
+
+
+}
