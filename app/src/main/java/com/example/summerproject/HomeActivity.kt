4ca36c9bdc152a6fef2d081f7e1156c6c343a2ee
BlancCarte <yusung0924@naver.com>
2021-07-13 00:38:48 +0900

    로그아웃 및 회원가입 공백 유효성검사 작성완료

--- app/src/main/java/com/example/summerproject/HomeActivity.kt
+++ app/src/main/java/com/example/summerproject/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.summerproject
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -17,12 +19,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.summerproject.R
 import com.example.summerproject.databinding.ActivityHomeBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+private var firebaseAuth: FirebaseAuth? = null
 
 class HomeActivity  : AppCompatActivity() {
-
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        firebaseAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
@@ -41,11 +46,10 @@ class HomeActivity  : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_mypage
             )
         )
-
-
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
@@ -54,16 +58,24 @@ class HomeActivity  : AppCompatActivity() {
     //액션버튼 클릭 했을 때
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item!!.itemId){
-            android.R.id.home->{ // 메뉴 버튼
-                Toast.makeText(applicationContext, "메뉴 이벤트 실행", Toast.LENGTH_SHORT).show()
+            R.id.action_search->{ //회원정보 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "회원정보 실행", Toast.LENGTH_SHORT).show()
                 return super.onOptionsItemSelected(item)
             }
-            R.id.action_search-> {
-                //검색 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "검색 이벤트 실행", Toast.LENGTH_LONG).show()
+            R.id.action_search-> { //설정 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "설정 실행", Toast.LENGTH_LONG).show()
+                return super.onOptionsItemSelected(item)
+            }
+            R.id.action_logout-> { //로그아웃 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "로그아웃 실행", Toast.LENGTH_LONG).show()
+                firebaseAuth!!.signOut();
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
                 return super.onOptionsItemSelected(item)
             }
             else -> return super.onOptionsItemSelected(item)
         }
     }
+   
+
 }
--- app/src/main/java/com/example/summerproject/SignUpActivity.kt
+++ app/src/main/java/com/example/summerproject/SignUpActivity.kt
@@ -20,176 +20,190 @@ private var firebaseFirestore: FirebaseFirestore? = null
 
 class SignUpActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseFirestore = FirebaseFirestore.getInstance()
 
-		firebaseAuth = FirebaseAuth.getInstance()
-		firebaseFirestore = FirebaseFirestore.getInstance()
+        val binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide();
 
-		val binding = ActivitySignUpBinding.inflate(layoutInflater)
-		setContentView(binding.root)
-		supportActionBar?.hide();
+        //이메일 유효성검사
+        binding.email.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var regex =
+                    Regex("[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}")
+                var email = binding.email.text.toString()
+                if (email.matches(regex)) {
+                    binding.emailTest.setTextColor(Color.parseColor("#369F36"))
+                    binding.emailTest.setText("이메일이 입력되었습니다.")
 
-		//이메일 유효성검사
-		binding.email.addTextChangedListener(object: TextWatcher{
-			override fun afterTextChanged(p0: Editable?) {
-				var regex = Regex("[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}")
-				var email = binding.email.text.toString()
+                }
+                else{
+                    binding.emailTest.setTextColor(Color.parseColor("#ff0000"))
+                    binding.emailTest.setText("이메일을 형식에 맞춰 입력해주세요")
 
-				if(email.matches(regex)){
-					binding.btnRegister.isEnabled = true
-					binding.emailTest.setTextColor(Color.parseColor("#369F36"))
-					binding.emailTest.setText("이메일이 입력되었습니다.")
-				}else{
-					binding.btnRegister.isEnabled = false
-					binding.emailTest.setTextColor(Color.parseColor("#ff0000"))
-					binding.emailTest.setText("이메일을 형식에 맞춰 입력해주세요")
+                }
 
-				}
-			}
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-		})
+            }
 
-		//닉네임 유효성검사
-		binding.nickname.addTextChangedListener(object: TextWatcher{
-			override fun afterTextChanged(p0: Editable?) {
-				var regex = Regex("[가-힣a-zA-Z0-9]{2,10}")
-				var nickname = binding.nickname.text.toString()
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-				if(nickname.matches(regex)){
-					binding.btnRegister.isEnabled = true
-					binding.nicknameTest.setTextColor(Color.parseColor("#369F36"))
-					binding.nicknameTest.setText("별명이 입력되었습니다.")
-				}else{
-					binding.btnRegister.isEnabled = false
-					binding.nicknameTest.setTextColor(Color.parseColor("#ff0000"))
-					binding.nicknameTest.setText("별명을 형식에 맞춰 입력해주세요.")
-				}
-			}
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
 
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-		})
+        //닉네임 유효성검사
+        binding.nickname.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var regex = Regex("[가-힣a-zA-Z0-9]{2,10}")
+                var nickname = binding.nickname.text.toString()
+               if(nickname.matches(regex)){
+                    binding.nicknameTest.setTextColor(Color.parseColor("#369F36"))
+                    binding.nicknameTest.setText("별명이 입력되었습니다.")
+                }
+                else {
+                    binding.nicknameTest.setTextColor(Color.parseColor("#ff0000"))
+                    binding.nicknameTest.setText("별명을 형식에 맞춰 입력해주세요.")
 
-		//비밀번호 -> 비밀번호 확인 검사
-		binding.password.addTextChangedListener(object : TextWatcher {
-			override fun afterTextChanged(p0: Editable?) {
-				var regex = Regex("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@!%*#?&]).{8,15}")
-				var pw = binding.password.text.toString()
+                }
+            }
 
-				if(pw.matches(regex)){
-					binding.btnRegister.isEnabled = true
-					binding.passwordCheckText.setTextColor(Color.parseColor("#369F36"))
-					binding.passwordCheckText.setText("비밀번호가 입력되었습니다.")
-					if (binding.passwordConfirm.text.toString()==(binding.password.text.toString())){
-						binding.btnRegister.isEnabled = true
-						binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
-						binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
-					}
-				}
-				else{
-					binding.btnRegister.isEnabled = false
-					binding.passwordCheckText.setTextColor(Color.parseColor("#ff0000"))
-					binding.passwordCheckText.setText("비밀번호를 형식에 맞춰 입력해주세요.")
-					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
-					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-				}
-			}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
 
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        //비밀번호 -> 비밀번호 확인 검사
+        binding.password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var regex = Regex("(?=.*[A-Za-z])(?=.*[0-9])(?=.*[@!%*#?&]).{8,15}")
+                var pw = binding.password.text.toString()
+               if (pw.matches(regex)) {
+                    binding.passwordCheckText.setTextColor(Color.parseColor("#369F36"))
+                    binding.passwordCheckText.setText("비밀번호가 입력되었습니다.")
+                    if (binding.passwordConfirm.text.toString() == (binding.password.text.toString())) {
+                        binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
+                        binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
 
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-		})
+                    }
+                }
+                    else {
+                    binding.passwordCheckText.setTextColor(Color.parseColor("#ff0000"))
+                    binding.passwordCheckText.setText("비밀번호를 형식에 맞춰 입력해주세요.")
+                    binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+                    binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
 
-		//비밀번호 확인 -> 비밀번호 검사
-		//TextWatcher : 원하는 규칙에 대해서 실시간으로 검증하기 위해
 
-		binding.passwordConfirm.addTextChangedListener(object : TextWatcher {
-			override fun afterTextChanged(p0: Editable?) {
-				var pw = binding.password.text.toString()
-				if(binding.passwordConfirm.text.toString()==(binding.password.text.toString())){
-					binding.btnRegister.isEnabled = true
-					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
-					binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
-				}else if(!(binding.passwordConfirm.text.toString()==(binding.password.text.toString()))){
-					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
-					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
-				}
-				else{
-					binding.btnRegister.isEnabled = false
-					binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
-					binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
-				}
-			}
+                }
 
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            }
 
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-		})
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-		//전화번호
-		binding.phoneNumber.addTextChangedListener(object : TextWatcher{
-			override fun afterTextChanged(p0: Editable?) {
-				var phnum = binding.phoneNumber.text.toString()
-				var regex = Regex("01[016789][0-9]{3,4}[0-9]{4}$")
-				if(phnum.matches(regex)){
-					binding.btnRegister.isEnabled = true
-					binding.phoneNumberTest.setTextColor(Color.parseColor("#369F36"))
-					binding.phoneNumberTest.setText("입력되었습니다.")
-				}
-				else{
-					binding.btnRegister.isEnabled = false
-					binding.phoneNumberTest.setTextColor(Color.parseColor("#ff0000"))
-					binding.phoneNumberTest.setText("핸드폰 형식이 아닙니다.")
-				}
-			}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
 
-			override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        //비밀번호 확인 -> 비밀번호 검사
+        //TextWatcher : 원하는 규칙에 대해서 실시간으로 검증하기 위해
+        binding.passwordConfirm.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var pw = binding.password.text.toString()
+                var pwconfirm = binding.passwordConfirm.text.toString()
+               if (binding.passwordConfirm.text.toString() == (binding.password.text.toString())) {
+                    binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#369F36"))
+                    binding.passwordConfirmCheckText.setText("비밀번호가 일치합니다.")
+                }
+                else if (!(binding.passwordConfirm.text.toString() == (binding.password.text.toString()))) {
+                    binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+                    binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
 
-			override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-		})
+                } else {
+                    binding.passwordConfirmCheckText.setTextColor(Color.parseColor("#ff0000"))
+                    binding.passwordConfirmCheckText.setText("비밀번호가 일치하지 않습니다.")
 
-		// 게터세터
-		data class UserDTO(
-			var uid: String? = null,
-			var email: String? = null,
-			var password: String? = null,
-			var nickname: String? = null,
-			var phoneNumber: String? = null
-		)
+                }
 
-		binding.btnRegister.setOnClickListener {
-			var userDTO = UserDTO()
-			userDTO.uid = firebaseAuth?.currentUser?.uid
-			userDTO.email = binding.email.text.toString()
-			userDTO.nickname = binding.nickname.text.toString()
-			userDTO.password = binding.password.text.toString()
-			userDTO.phoneNumber = binding.phoneNumber.text.toString()
+            }
 
-			firebaseAuth!!.createUserWithEmailAndPassword(userDTO.email!!, userDTO.password!!)
-				.addOnCompleteListener(this) {
-					if (it.isSuccessful) {
-						firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)
-							?.document(userDTO.nickname!!)?.set(userDTO)
-						val user = firebaseAuth!!.currentUser
-						user!!.sendEmailVerification()
-							.addOnCompleteListener { task ->
-								if (task.isSuccessful) {
-									Toast.makeText(this, "회원가입 완료, 인증 이메일이 발송되었습니다.", Toast.LENGTH_SHORT).show()
-									val intent = Intent(this, MainActivity::class.java)
-									startActivity(intent)
-								}
-							}
-					} else {
-						Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
-					}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-				}
-		}
-	}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int){}
+
+        })
+
+        //전화번호
+        binding.phoneNumber.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                var phnum = binding.phoneNumber.text.toString()
+                var regex = Regex("01[016789][0-9]{3,4}[0-9]{4}$")
+              if (phnum.matches(regex)) {
+                    binding.phoneNumberTest.setTextColor(Color.parseColor("#369F36"))
+                    binding.phoneNumberTest.setText("입력되었습니다.")
+                }
+                else {
+                    binding.phoneNumberTest.setTextColor(Color.parseColor("#ff0000"))
+                    binding.phoneNumberTest.setText("핸드폰 형식이 아닙니다.")
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+
+
+        // 게터세터
+        data class UserDTO(
+            var uid: String? = null,
+            var email: String? = null,
+            var password: String? = null,
+            var nickname: String? = null,
+            var phoneNumber: String? = null
+        )
+
+        binding.btnRegister.setOnClickListener {
+
+            var userDTO = UserDTO()
+            userDTO.uid = firebaseAuth?.currentUser?.uid
+            userDTO.email = binding.email.text.toString()
+            userDTO.nickname = binding.nickname.text.toString()
+            userDTO.password = binding.password.text.toString()
+            var passwordconfrim = binding.passwordConfirm.text.toString()
+            userDTO.phoneNumber = binding.phoneNumber.text.toString()
+            if(userDTO.email!!.isNotEmpty() && userDTO.nickname!!.isNotEmpty() &&userDTO.password!!.isNotEmpty()&& passwordconfrim!!.isNotEmpty()&& userDTO.phoneNumber!!.isNotEmpty() ){
+                firebaseAuth!!.createUserWithEmailAndPassword(userDTO.email!!, userDTO.password!!)
+                    .addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
+                            firebaseFirestore?.collection(firebaseAuth!!.currentUser!!.uid)
+                                ?.document(userDTO.nickname!!)?.set(userDTO)
+                            val user = firebaseAuth!!.currentUser
+                            user!!.sendEmailVerification()
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        Toast.makeText(
+                                            this,
+                                            "회원가입 완료, 인증 이메일이 발송되었습니다.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val intent = Intent(this, MainActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                }
+                        } else {
+                            Toast.makeText(this, "회원가입 실패, 이미 존재하는 이메일입니다.", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+            }else{
+                Toast.makeText(this, "빈칸을 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
 
 // 파이어베이스 잘 들어가는지 확인 (O)
