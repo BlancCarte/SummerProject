af46fe4c048b08121c473d25df524f21c88c0987
gyuho <gyuho0789@naver.com>
2021-07-16 10:52:37 +0900

    KKH modifyProfile1

--- app/src/main/java/com/example/summerproject/HomeActivity.kt
+++ app/src/main/java/com/example/summerproject/HomeActivity.kt
@@ -3,51 +3,49 @@ package com.example.summerproject
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.ActionBarContextView
-import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.summerproject.R
 import com.example.summerproject.databinding.ActivityHomeBinding
-import com.google.android.material.snackbar.Snackbar
+import com.example.summerproject.databinding.FragmentMypageBinding
+import com.example.summerproject.ui.dashboard.DashboardFragment
+import com.example.summerproject.ui.mypage.MypageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+
 
 private var firebaseAuth: FirebaseAuth? = null
 
-class HomeActivity  : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         firebaseAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
-
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
-        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_home_black_24dp)  // 왼쪽 버튼 아이콘 설정
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar.toolbar)
 
-        //val navView: BottomNavigationView = binding.navView
-
-        /*val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        val navView: BottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_mypage
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_mypage
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)*/
+        navView.setupWithNavController(navController)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -55,19 +53,31 @@ class HomeActivity  : AppCompatActivity() {
         return true
     }
 
+
     //액션버튼 클릭 했을 때
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item!!.itemId){
-            R.id.action_search->{ //회원정보 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "회원정보 실행", Toast.LENGTH_SHORT).show()
+        when (item!!.itemId) {
+            R.id.action_search -> { //검색 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "검색 실행", Toast.LENGTH_SHORT).show()
                 return super.onOptionsItemSelected(item)
             }
-            R.id.action_search-> { //설정 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "설정 실행", Toast.LENGTH_LONG).show()
+            R.id.action_notification -> { //알림 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "알림 실행", Toast.LENGTH_SHORT).show()
                 return super.onOptionsItemSelected(item)
             }
-            R.id.action_logout-> { //로그아웃 버튼 눌렀을 때
-                Toast.makeText(applicationContext, "로그아웃 실행", Toast.LENGTH_LONG).show()
+            /*
+            R.id.action_userinfo->{//회원정보 눌렀을 때
+                 Toast.makeText(applicationContext, "회원정보 실행", Toast.LENGTH_SHORT).show()
+                 return super.onOptionsItemSelected(item)
+             }
+             R.id.action_option->{//설정 눌렀을 때
+                 Toast.makeText(applicationContext, "설정 실행", Toast.LENGTH_SHORT).show()
+                 return super.onOptionsItemSelected(item)
+             }
+             */
+
+            R.id.action_logout -> { //로그아웃 버튼 눌렀을 때
+                Toast.makeText(applicationContext, "로그아웃 실행", Toast.LENGTH_SHORT).show()
                 firebaseAuth!!.signOut();
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
@@ -77,6 +87,11 @@ class HomeActivity  : AppCompatActivity() {
         }
     }
 
+    fun replaceFragment() {
+        val textFragment = MypageFragment()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_home, textFragment).commit()
 
-
+    }
 }
--- app/src/main/java/com/example/summerproject/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/summerproject/ui/mypage/MypageFragment.kt
@@ -1,13 +1,18 @@
 package com.example.summerproject.ui.mypage
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.summerproject.HomeActivity
+import com.example.summerproject.MainActivity
 import com.example.summerproject.R
 
 import com.example.summerproject.databinding.FragmentMypageBinding
@@ -16,31 +21,44 @@ class MypageFragment : Fragment() {
 
     private lateinit var mypageViewModel: MypageViewModel
     private var _binding: FragmentMypageBinding? = null
-
+    lateinit var mainActivity: HomeActivity
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity=context as HomeActivity
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_mypage,container, false)
         mypageViewModel =
             ViewModelProvider(this).get(MypageViewModel::class.java)
 
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textMypage
-        mypageViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return binding.root
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.profileModify.setOnClickListener(){
+            Toast.makeText(context,"123",Toast.LENGTH_SHORT).show()
+            mainActivity.replaceFragment()
+
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
