838043a9648c61045dd274c0539cf2a2c9e31356
Beomseok <beso1234@naver.com>
2021-08-02 22:06:17 +0900

    클릭리스너 만들기 위한 액티비티 생성(미완)

---
+++ app/src/main/java/com/example/summerproject/ui/home/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.summerproject.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.summerproject.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_detail)
+    }
+}
