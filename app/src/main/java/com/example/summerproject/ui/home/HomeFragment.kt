07771d784a1550de1c421fb7eddb7be2dc8db0c0
BlancCarte <yusung0924@naver.com>
2021-08-09 22:10:09 +0900

    홈프레그먼트 리싸이클러뷰 구현완료/리얼타임 데이터베이스 값 넣기/불러와서 리싸이클러뷰에 표시 완료

--- app/src/main/java/com/example/summerproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/summerproject/ui/home/HomeFragment.kt
@@ -1,116 +1,137 @@
 package com.example.summerproject.ui.home
 
-import android.content.res.Resources
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.summerproject.R
 import com.example.summerproject.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.example.summerproject.DBKey.Companion.DB_ARTICLES
+import com.example.summerproject.DBKey.Companion.DB_USERS
 
 
-class HomeFragment : Fragment() {
-    private val flowersList by viewModels<FlowersList> {
-        FlowersListFactory(this)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var articleDB: DatabaseReference
+    private lateinit var userDB: DatabaseReference
+    private lateinit var articleAdapter: ArticleAdapter
+
+    private val articleList = mutableListOf<ArticleModel>()
+
+    private val listener = object : ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+
+            articleList.add(articleModel) // 리스트에 새로운 항목을 더해서;
+            articleAdapter.submitList(articleList) // 어뎁터 리스트에 등록;
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+
     }
 
-    private var mBinding: FragmentHomeBinding? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentHomeBinding.inflate(inflater, container, false)
-        mBinding = binding
-        return mBinding?.root
-
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
     }
+
+    private var binding: FragmentHomeBinding? = null
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("sslee", "onViewCreated")
 
-        val flowersAdapter = FlowersAdapter { FlowerDiffCallback }
-        val concatAdapter = ConcatAdapter(flowersAdapter)
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
 
-        val recyclerView: RecyclerView = mBinding!!.recyclerView
-        recyclerView.adapter = concatAdapter
+        articleList.clear() //리스트 초기화;
 
-        flowersList.flowersLiveData.observe(viewLifecycleOwner, {
-            it?.let {
-                flowersAdapter.submitList(it as MutableList<Flower>)
+        initDB()
+
+        initArticleAdapter(view)
+
+        initArticleRecyclerView()
+
+        initFloatingButton(view)
+
+        // 데이터 가져오기;
+        initListener()
+    }
+
+    private fun initListener() {
+        articleDB.addChildEventListener(listener)
+    }
+
+    private fun initFloatingButton(view: View) {
+        // 플로팅 버튼;
+        binding!!.addFloatingButton.setOnClickListener {
+            context?.let {
+                if (auth.currentUser != null) {
+                    val intent = Intent(it, AddArticleActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_LONG).show()
+                }
             }
+        }
+    }
+
+    private fun initArticleRecyclerView() {
+        // activity 일 때는 그냥 this 로 넘겼지만 (그자체가 컨텍스트라서) 그러나
+        // 프레그 먼트의 경우에는 아래처럼. context
+        binding?:return
+
+        binding!!.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding!!.recyclerView.adapter = articleAdapter
+    }
+
+    private fun initArticleAdapter(view: View) {
+        articleAdapter = ArticleAdapter(onItemClicked = { articleModel ->
         })
     }
 
-    override fun onDestroyView() {
-        mBinding = null
-        super.onDestroyView()
+    private fun initDB() {
+        articleDB = Firebase.database.reference.child(DB_ARTICLES) // 디비 가져오기;
+        userDB = Firebase.database.reference.child(DB_USERS)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        articleDB.removeEventListener(listener)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+
+        articleAdapter.notifyDataSetChanged() // view 를 다시 그림;
+    }
+
+    private fun setArticleSample() {
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0", "AAA", 1000000, "5000원", ""))
+            add(ArticleModel("0", "BBB", 2000000, "10000원", ""))
+        })
+    }
+
 }
-
-
-    data class Flower(
-        val name: String,
-        @DrawableRes
-        val image: Int?
-    )
-
-    fun flowerList(resources: Resources): List<Flower> {
-        return listOf(
-            Flower(
-                name = resources.getString(R.string.flower1_name),
-                image = R.drawable.rose
-            ),
-            Flower(
-                name = resources.getString(R.string.flower2_name),
-                image = R.drawable.freesia
-            ),
-            Flower(
-                name = resources.getString(R.string.flower3_name),
-                image = R.drawable.lily
-            ),
-            Flower(
-                name = resources.getString(R.string.flower4_name),
-                image = R.drawable.sunflower
-            ),
-            Flower(
-                name = resources.getString(R.string.flower5_name),
-                image = R.drawable.peony
-
-            ),
-            Flower(
-                name = resources.getString(R.string.flower6_name),
-                image = R.drawable.daisy
-            ),
-            Flower(
-                name = resources.getString(R.string.flower7_name),
-                image = R.drawable.lilac
-            ),
-            Flower(
-                name = resources.getString(R.string.flower8_name),
-                image = R.drawable.marigold
-            ),
-            Flower(
-                name = resources.getString(R.string.flower9_name),
-                image = R.drawable.poppy
-            ),
-            Flower(
-                name = resources.getString(R.string.flower10_name),
-                image = R.drawable.daffodil
-            ),
-            Flower(
-                name = resources.getString(R.string.flower11_name),
-                image = R.drawable.dahlia
-            )
-        )
-    }
-
-
-
-
